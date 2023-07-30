db75c2efae5f0773e5a91c73d769be0df33b3dbb
gerst-416-dataviz <liam.gerst+dataviz@gmail.com>
2023-07-29 21:45:55 -0400

    Update splash screen

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ function startDataViz() {
 function showIntroText() {
     var centered = document.getElementById("centered");
     centered.innerHTML = "";
-    var lines = ["They just don't make movies like they used to...", "...or do they?", "Let's find out."];
+    var lines = ["They just don't make movies like they used to...", "...or do they?", "Have movies gotten better?", "Have they gotten longer?", "Let's find out."];
     lines.forEach((e, i) => {
         var span = document.createElement("span");
         console.log("created " + i);
         span.style.transition = "all .5s";
-        span.style.transitionDelay = (2 * i) + "s";
+        span.style.transitionDelay = (1.5 * i) + "s";
         span.style.opacity = 0;
         span.textContent = e;
         centered.appendChild(span);
@@ -20,7 +20,7 @@ function showIntroText() {
             span.style.opacity = 1;
             // console.log(i + " is now becoming visible");
             if (i == lines.length - 1) {
-                setTimeout(scene1StartButtonAppear, (2 * i + 1) * 1000);
+                setTimeout(scene1StartButtonAppear, (1.5 * i + 1) * 1000);
             }
         }, 10);
     });
@@ -31,9 +31,10 @@ function scene1StartButtonAppear() {
     var scene1Button = document.createElement("button");
     scene1Button.style.transition = "all .5s";
     scene1Button.style.opacity = 0;
-    scene1Button.innerHTML = "\u2192";
+    scene1Button.innerHTML = "Press to continue...\u2192";
     scene1Button.className = "niceButton";
     scene1Button.onclick = () => { console.log('yay'); window.location.href = "ratings.html"; };
+    centered.appendChild(document.createElement("br"));
     centered.appendChild(scene1Button);
     setTimeout(function() {
         scene1Button.style.opacity = 1;
